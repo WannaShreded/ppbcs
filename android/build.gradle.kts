5569770067e02c04b3cb3d41ef2c0fb4c7750e20
WannaShreded <mhijrithoriqmrf@gmail.com>
2026-05-30 10:50:24 +0700

    anu

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,14 +22,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:7.3.0'
-        classpath 'com.google.gms:google-services:4.3.15'
-    }
-}
